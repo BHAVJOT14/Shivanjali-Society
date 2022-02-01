1c8a394a3b1f4e4071094e4f504db43f6a679c62
bhavi140201 <bhavjotbhavi14@gmail.com>
2022-02-01 23:19:09 +0530

    made few changes left with few more

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " dotActive";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 3000); // Change image every 5 seconds
 }
 //------- slider ends -------
 
