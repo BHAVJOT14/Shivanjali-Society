bdc3c9e79979a51384c791beb5ef289f3c7f07c4
bhavi140201 <bhavjotbhavi14@gmail.com>
2022-02-03 19:35:36 +0530

    Made few changes of the directory added basic struture of all the pages

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-//------- navbar starts -------
-const toggle = document.getElementById("toggle");
-const navbar = document.getElementById("navbar");
-const header = document.getElementById("header");
-document.onclick = function (e) {
-  if (
-    e.target.id !== "header" &&
-    e.target.id !== "toggle" &&
-    e.target.id !== "navbar"
-  ) {
-    toggle.classList.remove("active");
-    navbar.classList.remove("active");
-  }
-};
-toggle.onclick = function () {
-  toggle.classList.toggle("active");
-  navbar.classList.toggle("active");
-};
-//------- navbar ends -------
-
 //------- slider starts -------
 var slideIndex = 0;
 showSlides();
